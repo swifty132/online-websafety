779e01afc877c0bf885dd6bb433506361cfb7f32
swifty132 <54196963+swifty132@users.noreply.github.com>
2021-06-12 22:16:18 +1000

    IT WORKS!!

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ const popData = {
             "name": "Tech Support Scam",
             "title": "Your PC has a virus!",
             "description": "A virus on your pc.."
+        },
+        "jeff": {
+            "name": "Tech Support Scam",
+            "title": "heya!",
+            "description": "A virus on your pc.."
+        },
+        "bob": {
+            "name": "Tech Support Scam",
+            "title": "sup",
+            "description": "A virus on your pc.."
         }
     };
 
-var details = popData;
+var items_left = popData;
+var details;
 
 function pressedButton() {
     //alert("MUWHAHAHAHA")
@@ -15,15 +26,23 @@ function pressedButton() {
 
 function on() {
     document.getElementById("popup").style.display = "block";
-    document.getElementById("p-title").innerHTML = popData[1]["title"];
+    document.getElementById("p-title").innerHTML = details["title"];
     console.log(document.getElementById("p-title").innerHTML);
     console.log(popData);
+    setup_popup();
 };
 
 function off() {
     document.getElementById("popup").style.display = "none";
 };
 
+function setup_popup() {
+    var keys = Object.keys(items_left);
+    var key_index = Math.floor(Math.random() * keys.length);
+    var rando_key = keys[key_index]
+    details = items_left[rando_key];
+}
+setup_popup();
 
 
 /*var element = document.getElementById("page");
