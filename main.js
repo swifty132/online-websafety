8b1d79917d226f0d27021ee06f21ad0dece97a6a
swifty132 <54196963+swifty132@users.noreply.github.com>
2021-06-12 14:19:19 +1000

    Put components into seperate files

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello there!")
+function buttonFunc() {
+    alert("MUWHAHAHAHA")
+
+}
+
+
+/*var element = document.getElementById("page");
+    var orig_content = element.innnerHTML;
+    var orig_css = element.style;
+    element.innerHTML = "YOU FOOL!!";
+    element.setAttribute("style", "font-size: 100px; font-style: bold; color: white; text-align: center; height: 100vh; display: flex; justify-content: center; align-items: center;");*/
