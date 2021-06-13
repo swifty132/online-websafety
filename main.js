1ff3e90abcb1824eb1aefe1591f51bf26a859d61
swifty132 <54196963+swifty132@users.noreply.github.com>
2021-06-13 19:39:23 +1000

    Made question system

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-const origData = {
+var origData = {
         "tech-support": {
             "name": "Tech Support Scam",
             "title": "Your PC has a virus!",
             "desc": "We have detected a virus on your pc!!",
             "opt-1": "Call the number",
-            "opt-2": "Close the tab"
+            "opt-2": "Close the tab",
+            "correct": 2,
+            "feedback": "this type of scam"
         },
         "fake-download": {
             "name": "Fake Download Virus",
             "title": "Your PC is unprotected!",
             "desc": "Your PC could be attacked at ANYTIME!",
             "opt-1": "Call the number",
-            "opt-2": "Close the tab"
+            "opt-2": "Close the tab",
+            "correct": 2,
+            "feedback": "this type of scam"
         },
         "Cookies": {
             "name": "Cookie Prompt",
             "title": "Accept Cookies to continue reading",
             "desc": "In order for you to continue reading you must accept cookies, otherwise we will close this tab.",
             "opt-1": "Call the number",
-            "opt-2": "Close the tab"        
+            "opt-2": "Close the tab",
+            "correct": 2,
+            "feedback": "this type of scam"
         }
     };
 
-var items_left = origData;
+var items_left = Object.assign({},origData);
 var details;
 
 function pressedButton() {
@@ -37,9 +43,6 @@ function on() {
     document.getElementById("opt-1").innerHTML = details["opt-1"];
     document.getElementById("opt-2").innerHTML = details["opt-2"];
 
-    console.log(document.getElementById("p-title").innerHTML);
-    console.log(origData);
-
     setup_popup();
 };
 
@@ -48,16 +51,32 @@ function off() {
 };
 
 function setup_popup() {
+    console.log(origData)
+    console.log(items_left)
     var keys = Object.keys(items_left);
-    var key_index = Math.floor(Math.random() * keys.length);
-    var rando_key = keys[key_index];
-    details = items_left[rando_key];
-    delete items_left[rando_key];
+    console.log(keys.length)
+    if (keys.length > 0) {
+        console.log("new");
+        var key_index = Math.floor(Math.random() * keys.length);
+        var rando_key = keys[key_index];
+        details = items_left[rando_key];
+        delete items_left[rando_key];
+        console.log(origData)
+        console.log(items_left)
+    }
+    else {
+        console.log("reset");
+        items_left = Object.assign({},origData);
+    }
 }
 
 function opt_pressed(id) {
-    console.log(id);
+    //console.log(id);
     off();
+}
+
+function resume() {
+    
 }
 setup_popup();
 
