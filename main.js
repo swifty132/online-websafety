61a0e37203e619ac712c0d0546d84d12a3aa69a9
swifty132 <54196963+swifty132@users.noreply.github.com>
2021-06-14 21:34:13 +1000

    added in some request feature or something
    Made it so that the json data was pulled from the file instead or a js var.
    
    TLDR; system now sends a request or something demanding data

--- main.js
+++ main.js
@@ -1,54 +1,7 @@
-var origData = {
-	"tech-support": {
-		"name": "Tech Support Scam",
-		"title": "Your PC has a virus!",
-		"desc": "We have detected a virus on your pc!! Please call our certified windows support technician at 1300-764-726 . This is an URGENT issue, immediate action is required or else your PC will be shutdown! (5 minute countdown with a loud voice saying: URGENT)",
-		"opt-1": "Call the number",
-		"opt-2": "Close the tab/popup",
-		"correct": 2,
-		"feedback": "This type of popup is nothing more than an ad, and is known as a tech support or remote access scam. If you encounter this, close the tab/window/popup, and if the page you were visting is a sketchy one don't revisit it. Send the details of the encounter to https://www.scamwatch.gov.au/report-a-scam or https://www.econsumer.gov/#crnt (if you are outside australia). <br><br>The way this scam works is that someone makes an ad/popup that tricks the target into thinking they have to contact the number and get a problem resolved, the people you call pretend to be from a large company, and will get you to allow them access to your PC claiming it is a 'secure server link' or the sorts. From here they will convince you that you do indeed have the problem and it needs resolving. They may then either 'fix' your PC, charging you upwards of a few hundred dollars, or install malicious content, they may even do both. <br><br>Variations: pretending to be a notification from a service like amazon, your bank, etc (further explained in another question). It may also simply have a link to some software for you to download that will 'clean' your PC but this is actually malicious content itself."
-	},
-
-	"fake-download": {
-		"name": "Fake Download Malware",
-		"title": "Your PC is unprotected!",
-		"desc": "Your PC could be attacked at ANYTIME!! You may even have malware on your PC right now! Download our software here (sketchy link) to remove any viruses you may have and protect yourself in the future! Out software is 100% FREE!!",
-		"opt-1": "Close the tab/popup",
-		"opt-2": "Click the link <br>and download the software",
-		"correct": 1,
-		"feedback": "This scam works by getting the user to download some program, it pretends that it will keep you safe from malicious software, but in fact it is malicious itself. If you see this scam you should simply close the tab/window/popup. You should only be downloading trusted known antivirus software, and when doing so either make sure you get it from the official website of said software (e.g. Only download AVG antivirus from avg.com), or get a trusted company (such as JB-Hi-FI) to install it for you and give you an expert recommendation on what one you should get. <br><br>The scam works by getting you to buy a fake copy of a legitiment piece of software, that will actually turn out to be a virus. It may also require you to pay for it, it may say that it's on sale and is really cheap, or have a cost to 'unlock' certain features. <br><br>Variations: it may pretend to be a free version of a game, it may have a link to their 'online scanner' which will inform you that you do indeed have malware and need to call their tech support or download their antivirus software (it is impossible to scan a PC properly from a website)"
-	},
-
-	"cookies": {
-		"name": "Cookie Prompt",
-		"title": "Accept Cookies to continue reading",
-		"desc": "In order for you to continue reading you must accept cookies, otherwise we will close this tab. We use cookies in order to delivier content customised for you, as well as vital functions of our site.",
-		"opt-1": "Turn off your pc and cut your internet connection",
-		"opt-2": "Press 'acccept cookies'",
-		"correct": 2,
-		"feedback": "Despite the fact this may seem like a scam or virus, 'cookies' are a completly safe. All they are is a text file on your browser that the website can access later to get information about you. Only the website that made the set of cookies may access and alter it. Despite it sounding like it may store sensitive info, cookies only contains info that the website put there. These are actually an inportant part of a website, imagine you are using YouTube in your PC browser, if you didnt have cookies then every time you opened a new tab you would need to login again. The cookies for youtube contains your user ID cookie, which is the authentication token your PC sends to infrom sites like YouTube who you are. <br><br>The only thing clicking 'accept cookies' does is let the website manage a cookie file, which only contains info it provides (it cannot rip info off your pc), this is normally a perfectly fine thing to do. The only downside can be that you may get ads targeted towards you, but this helps the website opperate and run, and ultimately whether you accept cookies or not is up to you. <br><br>Variations: some sites may request you to disable ad-block, however most will just say this as a friendly request and you can normally just close the popup."
-	},
-
-	"amazon-fraud": {
-		"name": "Amazon Fraud Message",
-		"title": "We have detected fraud on your amazon account!!",
-		"desc": "We have suspended an order for an iPhone 12 pro, that was placed under your account. We detected it as a fraudulent charge, if you did indeed order this ignore this message, if you didn't order it please call up 1300-566-391 and a customer care member will be with you shortly to cancel the order and refund your $800.",
-		"opt-1": "Close the tab/popup",
-		"opt-2": "Ring the number and follow the instructions",
-		"correct": 1,
-		"feedback": "As online shopping becomes more and more popular this scam has become more preeminent. It claims to be an alert from a company such as Amazon or Ebay, etc. It says that they detected a fraudulent charge against your account, which is usually a product worth a lot of money, and in order to confirm the decline you must contact them to get a refund. If you see this kind of scam you should close the tab or popup and report the scam to <a href='https://www.scamwatch.gov.au/report-a-scam'>Scam Watch</a> or <a href='https://www.econsumer.gov/#crnt'>Econsumer</a> (if you are outside australia)."
-	},
-
-	"key": {
-		"name": "name",
-		"title": "title",
-		"desc": "desc",
-		"opt-1": "choice",
-		"opt-2": "choice",
-		"correct": 12,
-		"feedback": "feedback"
-	}
-};
+const xhttp = new XMLHttpRequest();
+var origData;
+var items_left;
+var details;
 
 /*
 "key": {
@@ -60,10 +13,6 @@ var origData = {
 		"correct": 12,
 		"feedback": "feedback"
 	}*/
-
-
-var items_left = Object.assign({},origData);
-var details;
 
 function pressedButton() {
     document.getElementById("but1").style.visibility = "hidden";
@@ -142,8 +91,28 @@ function reset() {
     document.getElementById("but1").style.visibility = "visible";
 }
 
-var keys = Object.keys(items_left);
-var key_index = Math.floor(Math.random() * keys.length);
-var rando_key = keys[key_index];
-details = items_left[rando_key];
-delete items_left[rando_key];
+// Define a callback function
+xhttp.onload = function() {
+    var tempData;
+    tempData = JSON.parse(xhttp.responseText);
+    console.log("DONE!")
+    origData = Object.assign({},tempData);
+    console.log(tempData)
+    console.log(origData)
+    start();
+}
+
+// Send a request
+xhttp.open("GET", "popups.json", true);
+xhttp.send();
+
+
+function start() {
+    items_left = Object.assign({},origData);
+
+    var keys = Object.keys(items_left);
+    var key_index = Math.floor(Math.random() * keys.length);
+    var rando_key = keys[key_index];
+    details = items_left[rando_key];
+    delete items_left[rando_key];
+}
